2ff9602db9432921595abd28ee44c812f4228cda
Joe <zsklari11@gmail.com>
2020-12-08 18:59:29 +0100

    refresh

--- js/main.js
+++ js/main.js
@@ -5,3 +5,23 @@ const cardsFront = document.querySelectorAll('.flip-box-front');
 const cardsBack = document.querySelectorAll('.flip-box-back');
 const deckOfCards = ['sponge', 'patrick', 'squid', 'crab', 'sandy'];
 const doubleDeck = deckOfCards.concat(deckOfCards);
+
+let blockClicks = false;
+
+
+const cardClick = (ev) => {
+    ev.currentTarget.classList.toggle('flipped');
+    const flippedCards = document.querySelectorAll('.card.flipped');
+    if (flippedCards.length > 1) {
+        blockClicks = true;
+        const to = setTimeout ()
+    }
+}
+
+const checkPair = () => {
+    const firstCardIcon = document.querySelector('.card.flipped i');
+    if (firstCardIcon) {
+        const firstIconClass = firstCardIcon.className.split(' ');
+        if (document.querySelectorAll())
+    }
+}
